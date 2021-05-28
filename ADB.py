5550d6b243dd77e8d9bee02064f2acf4d2704e4d
cnnrsnc <84979649+cnnrsnc@users.noreply.github.com>
2021-05-28 13:42:27 -0400

    Update ADB.py

--- ADB.py
+++ ADB.py
@@ -2,7 +2,7 @@ from ppadb.client import Client as AdbClient
 
 
 client = AdbClient(host="127.0.0.1", port=5037)
-device = client.device("emulator-5554")
+device = client.device("ce0919392d8ea714017e")
 
 
 def dump_logcat(connection, line_counter=30):
@@ -10,17 +10,19 @@ def dump_logcat(connection, line_counter=30):
         data = connection.read(1024)
         line = data.decode('utf-8')
         logcat = open("logcat.txt", "a+")
-        logcat.append(line + "\n\n")
+        logcat.write(line + "\n\n")
         line_counter -= 1
         if line_counter == 0:
             wholethang = logcat.readlines()
-            line_counter += 30
             if wholethang.find("Application is not responding") != True:
                 logcat.close()
                 logcat = open("logcat.txt", "w")
                 logcat.write()
+            else:
+                break
         if not data:
             break
         print(data.decode('utf-8'))
 
 device.shell("logcat", handler=dump_logcat)
+print(device.is_installed("com.snc.trax"))
